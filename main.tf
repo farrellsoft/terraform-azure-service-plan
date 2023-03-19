6bc8ecc7c05b6d8321977015a0d7f94c3c0b1cf2
Jason Farrell <imjason@gmail.com>
2023-03-19 15:21:37 -0400

    converted to azapi

--- main.tf
+++ main.tf
@@ -1,18 +1,51 @@
 
+terraform {
+  required_providers {
+    azapi = {
+      source = "Azure/azapi"
+    }
+  }
+}
+
+data azurerm_client_config current {}
 module "resource-naming" {
   source  = "app.terraform.io/Farrellsoft/resource-naming/azure"
-  version = "0.0.7"
-
+  version = "0.0.9"
+  
   application         = var.application
   environment         = var.environment
   instance_number     = var.instance_number
 }
 
-resource azurerm_service_plan this {
-  resource_group_name       = var.resource_group_name
-  location                  = var.location
-  name                      = module.resource-naming.service_plan_name
+locals {
+  resource_group_id     = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}"
+}
 
-  os_type                   = var.os_type
-  sku_name                  = var.sku_name
+#resource azurerm_service_plan this {
+#  resource_group_name       = var.resource_group_name
+#  location                  = var.location
+#  name                      = module.resource-naming.service_plan_name
+#
+#  os_type                   = var.os_type
+#  sku_name                  = var.sku_name
+#}
+
+resource azapi_resource this {
+  type            = "Microsoft.Web/serverfarms@2022-03-01"
+  name            = module.resource-naming.service_plan_name
+  parent_id       = local.resource_group_id
+  location        = var.location
+  
+  body            = jsonencode({
+    kind      = "elastic"
+    sku       = {
+      family    = "EP"
+      name      = "EP1"
+      size      = "EP1"
+      tier      = "ElasticPremium"
+    }
+    properties  = {
+      reserved        = true
+    }
+  })
 }
--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 
 output service_plan_resource_id {
-  value       = azurerm_service_plan.this.id
+  value       = azapi_resource.this.id
   description = "The ID of the App Service Plan."
 }
 
 output service_plan_name {
-  value       = azurerm_service_plan.this.name
+  value       = azapi_resource.this.name
   description = "The name of the App Service Plan."
 }
